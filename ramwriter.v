ef38e5c8810082e8c8b7970510145918a73c9744
ozoralabs <matt.morra@ozoralabs.com>
2020-02-03 15:20:41 -0500

    fixed super fast ram write bug

--- ramwriter.v
+++ ramwriter.v
@@ -7,7 +7,8 @@ always #1 r_fakeclock <= ~r_fakeclock;
 parameter INIT_STATE = 3'b000;
 parameter START_WRITE = 3'b001;
 parameter END_WRITE = 3'b010;
-parameter STOP_ALL = 3'b011;
+parameter WAIT_STATE = 3'b011;
+parameter STOP_ALL = 3'b100;
 
 reg[3:0] current_state = INIT_STATE;
 
@@ -42,7 +43,7 @@ begin
 	 START_WRITE:
 	 begin
 	   r_wbit <= 1;
-      r_address <= r_address + 1; 
+      //r_address <= r_address + 1; 
 		
 	   r_data_word1 <= r_data_word1 + 4;
 	   r_data_word2 <= r_data_word2 + 4;
@@ -50,17 +51,35 @@ begin
 	   r_data_word4 <= r_data_word4 + 4;
 		
 		current_state <= END_WRITE;
+		
+		if (r_address == 14'h3FFF)
+		  r_address <= 1;
+		else
+		  r_address <= r_address + 1;
+		
 	 end
 	 
 	 END_WRITE:
 	 begin
 	 	r_wbit <= 0;
-		if(r_address[13] == 1)
-		  current_state <= STOP_ALL;
-		else
-		  current_state <= START_WRITE;
+		//if(r_address[13] == 1)
+		//  current_state <= STOP_ALL;
+		//else
+		current_state <= WAIT_STATE;
+		  
+		  
 	 end
 	 
+	 WAIT_STATE:
+	 begin
+	   if(clk_ctr >= 10) //Start with 100samples/sec for testing
+           begin
+		  current_state <= START_WRITE;
+		  clk_ctr <= 0;
+	   end
+           else
+		  clk_ctr <= clk_ctr + 1;
+	 end
 	 
 	 
 	 STOP_ALL:
